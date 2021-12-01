01602ffae6296d38402511a075827b7cda618293
Hung Cao <hungcao@Hungs-MacBook-Pro.local>
2021-12-01 07:59:33 +0700

    update

--- static/js/42.8cd87559.chunk.js
+++
@@ -1,1 +0,0 @@
-(this["webpackJsonpreact-antd-admin"]=this["webpackJsonpreact-antd-admin"]||[]).push([[42],{903:function(t,n,c){"use strict";c.r(n);var e=c(0),a=c.n(e),i=c(45),r=c(25),u=c(14);n.default=function(){return a.a.useEffect((function(){return function(){Object(r.f)()}}),[]),Object(u.b)(u.a,{children:Object(u.b)(i.c,{})})}}}]);
--- static/js/48.a8c2477d.chunk.js
+++
@@ -1,1 +0,0 @@
-(this["webpackJsonpreact-antd-admin"]=this["webpackJsonpreact-antd-admin"]||[]).push([[48],{896:function(t,n,a){"use strict";a.r(n);a(0);var c=a(45),e=a(14);n.default=function(){return Object(e.b)(e.a,{children:Object(e.b)(c.c,{})})}}}]);
